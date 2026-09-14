cfc6cf7b02e0661548248fa96a9aff64384af3a1
arthursensai <mohamedaytsidibah@gmail.com>
2026-09-14 20:43:40 +0100

    added the base of the AI feature

---
+++ src/features/ai/component/aiBot.tsx
@@ -0,0 +1,11 @@
+import { Input } from "@/components/ui/input";
+import { useForm } from "@tanstack/react-form";
+
+const aiBot = () => {
+	const { mutate } = useAskAi();
+	return (
+		<section>
+			
+		</section>	
+	)
+}
