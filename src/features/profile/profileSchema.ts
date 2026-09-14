c08d07dddb2a98b3b814e87d06c39d6e4859c9dd
Mohammed Ait sidi bah <mohamedaytsidibah@gmail.com>
2026-09-14 11:51:46 +0100

    Make image_id optional and nullable in profileSchema

--- src/features/profile/profileSchema.ts
+++ src/features/profile/profileSchema.ts
@@ -40,7 +40,7 @@ export const ProfileSchema = z.object({
       "Only JPEG, PNG, WebP, GIF and SVG files are accepted.",
     )
     .nullable(),
-  image_id: z.uuid(),
+  image_id: z.uuid().nullable().optional(),
 });
 
 export type onBoardingSchemaType = z.infer<typeof OnBoardingSchema>;
