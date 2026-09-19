02f6df90fcfee0875af4b533330e148dac87db14
Mohammed Ait sidi bah <mohamedaytsidibah@gmail.com>
2026-09-19 20:01:05 +0100

    Delete .ai-feature-backup-20260914-215255 directory

--- .ai-feature-backup-20260914-215255/app/dashboard/documents/[id]/read/askAi/page.tsx
+++
@@ -1,9 +0,0 @@
-const page = () => {
-	return (
-		<section>
-			<p>here you can ask the ai</p>
-		</section>
-	)
-}
-
-export default page;
--- .ai-feature-backup-20260914-215255/lib/ai/index.ts
+++
@@ -1,5 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-
-const ai = new GoogleGenAI({});
-
-export default ai;
